c493372166c15dd3513b3f80eec93f29bb02d87c
miloszm <milosz.muszynski@digital.hmrc.gov.uk>
2019-12-17 18:06:52 +0000

    more feeOnlyTransaction tests

--- test/model/TransactionSpec.scala
+++ test/model/TransactionSpec.scala
@@ -3,15 +3,21 @@ package model
 import org.scalatest.{Matchers, WordSpec}
 
 class TransactionSpec extends WordSpec with Matchers {
+  val transactionTime = 500
+  val blockTime = 3000
 
   "fee only transaction" should {
-    val transaction = JsonTransaction(Seq(JsonInput(Some(JsonOutput(Some(30000))))), Seq(JsonOutput(Some(26000))), 0L, 0, 0, "abc", 220, 0)
-    val feeOnlyTransaction = transaction.toFeeOnlyTransaction(600000l,0,0)
+    val transaction = JsonTransaction(Seq(JsonInput(Some(JsonOutput(Some(30000))))), Seq(JsonOutput(Some(26000))), 0L, 0, 0, "abc", 220, transactionTime)
+    val feeOnlyTransaction = transaction.toFeeOnlyTransaction(600000l,0,blockTime)
     "calculate fees" in {
       feeOnlyTransaction.fees shouldBe 4000
     }
     "calculate fee per byte" in {
       feeOnlyTransaction.feePerByte shouldBe 18
+    }
+    "calculate age and block age" in {
+      feeOnlyTransaction.age shouldBe (blockTime-transactionTime)
+      feeOnlyTransaction.ageInBlocks shouldBe 4
     }
   }
 
