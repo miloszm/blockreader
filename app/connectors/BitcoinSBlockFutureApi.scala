6fd1a06ae70660f72721e909211b95910860227f
Milosz Muszynski <miloszm@Miloszs-Mini.home>
2020-01-16 21:04:30 +0000

    stream for btc-s trans read

--- app/connectors/BitcoinSBlockFutureApi.scala
+++ app/connectors/BitcoinSBlockFutureApi.scala
@@ -1,12 +1,17 @@
 package connectors
 
 import java.net.URI
+import java.util.concurrent.TimeUnit
 
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, ThrottleMode}
+import akka.stream.scaladsl.{Sink, Source}
 import cats.data.Validated
 import cats.data.Validated.{Invalid, Valid}
 import model.BlockReaderError
-import model.json.{JsonBlock, JsonInput, JsonOutput, JsonTransaction}
+import model.json._
 import org.bitcoins.core.crypto.DoubleSha256DigestBE
+import org.bitcoins.core.protocol.transaction.Transaction
 import org.bitcoins.rpc.jsonmodels.GetRawTransactionVin
 
 import scala.concurrent.duration._
@@ -17,6 +22,9 @@ object BitcoinSBlockFutureApi extends App {
   import org.bitcoins.core.config._
   import org.bitcoins.rpc.client.common._
   import org.bitcoins.rpc.config._
+  implicit val system = ActorSystem("blockreader")
+  implicit val materializer = ActorMaterializer()
+
 
   val username = "foo" //this username comes from 'rpcuser' in your bitcoin.conf file
   val password = "bar" //this password comes from your 'rpcpassword' in your bitcoin.conf file
@@ -62,25 +70,32 @@ object BitcoinSBlockFutureApi extends App {
     val blockFuture = rpcCli.getBlockRaw(h)
     var i = 0
     val blockResponse = Await.result(blockFuture, 20 seconds)
-//    blockFuture.map { blockResponse =>
-      val jsonTransactions = blockResponse.transactions.map { t =>
-        val transactionFuture = rpcCli.getRawTransaction(t.txIdBE)
-        val transaction = Await.result(transactionFuture, 20 seconds)
-        print("."); i = i + 1
-        if (i % 200 == 0) println
-        val inputs = processTransactionInputs(rpcCli, transaction.vin)
-        val outputs = transaction.vout.map{ o =>
+
+    val transactionsSource = Source.apply[Transaction](blockResponse.transactions.toVector)
+      //.throttle(50, FiniteDuration(1, TimeUnit.SECONDS), 50, ThrottleMode.Shaping)
+
+    val jsonTransactionsFuture = transactionsSource.mapAsync(parallelism = 8) { t =>
+      rpcCli.getRawTransaction(t.txIdBE).map { transactionResult =>
+        val nOfInputs = transactionResult.vin.size
+        print(if (nOfInputs <= 1) "." else s"[$nOfInputs]")
+        i = i + 1
+        if (i % 100 == 0) println
+        if (i % 1000 == 0) println(s"done:$i")
+        val inputs = processTransactionInputs(rpcCli, transactionResult.vin)
+        val outputs = transactionResult.vout.map { o =>
           JsonOutput(Some(o.value.satoshis.toLong), Some("" + o.scriptPubKey.addresses), Some(o.scriptPubKey.asm))
         }
-        JsonTransaction(inputs, outputs, 0, inputs.size, outputs.size, transaction.hash.hex, transaction.size, transaction.time.map(_.toLong).getOrElse(0L))
+        JsonTransaction(inputs, outputs, 0, inputs.size, outputs.size, transactionResult.hash.hex, transactionResult.size, transactionResult.time.map(_.toLong).getOrElse(0L))
       }
-      val fee = jsonTransactions.map{ t =>
+    }.runWith(Sink.seq)
+    jsonTransactionsFuture.map { jsonTransactions =>
+      val fee = jsonTransactions.map { t =>
         t.inputs.flatMap(_.prev_out).flatMap(_.value).sum - t.out.flatMap(_.value).sum
       }.sum
-      Future.successful(Valid(JsonBlock(fee, blockHeight, blockResponse.txCount.toInt, jsonTransactions, blockResponse.blockHeader.time.toInt)))
-//    }.recover{ case e =>
-//      Invalid(BlockReaderError(1, e.getMessage))
-//    }
+      Valid(JsonBlock(fee, blockHeight, blockResponse.txCount.toInt, jsonTransactions, blockResponse.blockHeader.time.toInt))
+    }.recover{ case e =>
+    Invalid(BlockReaderError(1, e.getMessage))
+    }
   }
 
   val r = Await.result(getMhmBlock("00000000000000000010b5ded3990b193dbc359cb5525a2e95e94491f1dea590", 0), 500 seconds)
